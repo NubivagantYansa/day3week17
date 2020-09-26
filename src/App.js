825febf682357e0cc4757611b79b6b6586184cdc
dreazz <agustinsilveira.96@gmail.com>
2020-09-26 14:02:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
+import 'bulma/css/bulma.css';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import foodList from './foods.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    foodList,
+    foodName: "",
+    foodCalories: "",
+    foodImage: "",
+    searchParams: "",
+    isFormDisplayed: false,
+    todayFood: []
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  toggleForm = () => {
+    this.setState({
+      isFormDisplayed: !this.state.isFormDisplayed
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    const { foodName, foodCalories, foodImage } = this.state;
+    const newFoodArray = [{ name: foodName, calories: foodCalories, image: foodImage }, ...this.state.foodList];
+    this.setState({
+      foodList: newFoodArray,
+      foodName: "",
+      foodCalories: "",
+      foodImage: "",
+      isFormDisplayed: false
+    })
+  }
+
+  addQuantity = (foodObject) => {
+    let todayFood = this.state.todayFood.slice();
+    let found = todayFood.find(element => element.name === foodObject.name);
+
+    foodObject.calories *= foodObject.quantity;
+    if (found) {
+      found.quantity += foodObject.quantity;
+      found.calories += foodObject.calories;
+    } else {
+      todayFood.push(foodObject)
+    }
+    this.setState({
+      todayFood
+    })
+  }
+
+  deleteFood = (foodName) => {
+    const newFoodArray = this.state.foodList.filter((element) => element.name !== foodName)
+    this.setState({
+      foodList: newFoodArray
+    })
+  }
+
+  render() {
+    const totalCalories = this.state.todayFood.reduce((acc, element) => acc + element.calories, 0)
+    return (
+      <div className="App">
+        <button onClick={this.toggleForm}> Add food </button>
+        { this.state.isFormDisplayed &&
+          <form onSubmit={this.handleOnSubmit}>
+            <label> Name</label>
+            <input name="foodName" placeholder="Lasagna" value={this.state.foodName} onChange={this.handleOnChange}></input>
+            <label> Calories </label>
+            <input name="foodCalories" placeholder="23" value={this.state.foodCalories} onChange={this.handleOnChange}></input>
+            <label> Image url</label>
+            <input name="foodImage" placeholder="url" value={this.state.foodImage} onChange={this.handleOnChange}></input>
+            <button> Create food </button>
+          </form>
+        }
+        <div className="field">
+          <div className="control">
+            <input className="input is-primary" type="text" placeholder="Pizza" name="searchParams" value={this.state.searchParams} onChange={this.handleOnChange} />
+          </div>
+        </div>
+        <div className="food-container">
+          <div>
+            {this.state.foodList.filter(element => element.name.toLocaleLowerCase().includes(this.state.searchParams.toLocaleLowerCase())).map((element, idx) => <FoodBox key={idx} food={element} deleteFood={this.deleteFood} addQuantity={this.addQuantity} />)}
+          </div>
+          <div>
+            <h1> Today's food</h1>
+            <ul>
+              {this.state.todayFood.map((element, index) => {
+                return (
+                  <li key={index}>
+                    {element.quantity} {element.name} = {element.calories} Cal
+                  </li>
+                )
+              })}
+              <p> Total: {totalCalories} calories</p>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
